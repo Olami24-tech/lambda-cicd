20a8b5b6417c642bfd7f4feef5c4850a2526c21c
Yusuf Oyedele <ola@olaservice.co.uk>
2026-07-22 02:01:27 +0100

    updated

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return{
         'statuscode': 200,
-        'body': json.dumps('Hello updated lambda from vscode')
+        'body': json.dumps('Hello CICD updated lambda from vscode')
     }
